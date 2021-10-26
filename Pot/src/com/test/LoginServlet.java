526bc6b07e7a241f7d86fffc331e226bdd84868c
smhrd <smhrd@59.0.100.4>
2021-10-26 11:12:00 +0900

    - 첫번째 commit

--- Pot/src/com/test/LoginServlet.java
+++ Pot/src/com/test/LoginServlet.java
@@ -1,4 +1,4 @@
-package com.index;
+package com.test;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class index
+ * Servlet implementation class LoginServlet
  */
-@WebServlet("/index")
-public class index extends HttpServlet {
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * Default constructor. 
      */
-    public index() {
+    public LoginServlet() {
         // TODO Auto-generated constructor stub
     }
 
