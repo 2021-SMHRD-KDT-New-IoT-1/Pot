05b64230eb9f3df238b38e791bd80307b0d93a68
smhrd <smhrd@121.147.0.190>
2021-10-29 15:37:18 +0900

    -사진파일 이름 영문으로 변경 로그인페이지 - LoginPage 메인이미지3(식물든남자) -Planthold 메인이미지4(화분) - MainImage4 수정됨 - LOgin 햇빛이비치는화분 -MainSun

--- Pot/src/com/test/LoginServlet.java
+++ Pot/src/com/test/LoginServlet.java
@@ -7,25 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginServlet
- */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
     public LoginServlet() {
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 }
