de8eb952d1a10bc4da424fbc775b272977dd6beb
smhrd <smhrd@59.0.100.4>
2021-11-03 15:12:33 +0900

    - 펌프 작동값을 0으로 되돌리는 코드

--- Pot/src/arduinosensor/GetSensor_b.java
+++ Pot/src/arduinosensor/GetSensor_b.java
@@ -24,6 +24,10 @@ public class GetSensor_b extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.print(result);
 		System.out.println(result);
+		int value = Integer.parseInt(result);
+		vo = dao.update_b(value);
+		out.print(value);
+		System.out.println(value);
 	
 	}
 }
